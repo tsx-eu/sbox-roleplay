1fe02a9c24dae478f9601a53cf33ca371ac5084a
Jonathan Lagneau <lagneaujonathan@gmail.com>
2021-11-13 14:20:15 +0100

    Add dal for inventory

---
+++ server/DAL/Repository/SItemRepository.cs
@@ -0,0 +1,91 @@
+﻿using charleroi.server.POCO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using charleroi;
+using Sandbox;
+using System.Text.Json;
+
+namespace charleroi.server.DAL.Repository
+{
+	class SItemRepository : IRepository<SItem>
+	{
+		public SItemRepository()
+		{
+
+		}
+
+		public bool Delete( SItem entity )
+		{
+			var res = CRUDTools.GetInstance().Del( "item", entity.Id ).Result;
+			if(res.Error != "" )
+			{
+				Log.Error( res.Error );
+				return false;
+			}
+			return true;
+		}
+
+		public SItem Get( ulong id )
+		{
+			var req = CRUDTools.GetInstance().Get( "item", id );
+
+			var res = req.Result;
+			if ( res.Error != "" )
+			{
+				Log.Error( res.Error );
+				return null;
+			}
+			var resPlayer = JsonSerializer.Deserialize<SItem>( res.Data.GetRawText() );
+			return resPlayer;
+		}
+
+		public IList<SItem> GetAll()
+		{
+			var req = CRUDTools.GetInstance().GetAll( "item" );
+			var res = req.Result;
+			if ( res.Error != "" )
+			{
+				Log.Error( res.Error );
+				return null;
+			}
+			var resMap = JsonSerializer.Deserialize<CRUDGetAllData>( res.Data.GetRawText() );
+			var SPlyList = new List<SItem>();
+			foreach (var elem in resMap.Data )
+			{
+				var SPly = JsonSerializer.Deserialize<SItem>( elem.Value.GetRawText() );
+				SPlyList.Add( SPly );
+			}
+			return SPlyList;
+		}
+
+		public bool Insert( SItem entity )
+		{
+			var res = CRUDTools.GetInstance().Set( "item", entity.Id, entity ).Result;
+			if ( res.Error != "" )
+			{
+				Log.Error( res.Error );
+				return false;
+			}
+			return true;
+		}
+
+		public void Save()
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool Update( SItem entity )
+		{
+			var res = CRUDTools.GetInstance().Set( "item", entity.Id, entity ).Result;
+			if ( res.Error != "" )
+			{
+				Log.Error( res.Error );
+				return false;
+			}
+			return true;
+		}
+	}
+}
