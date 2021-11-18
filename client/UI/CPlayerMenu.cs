7240ccb2b6e0d0970021d110bd7b08dd774517c6
Messorem <jeromegardette@gmail.com>
2021-11-18 18:03:14 +0100

    test

--- client/UI/CPlayerMenu.cs
+++ client/UI/CPlayerMenu.cs
@@ -6,23 +6,41 @@ using System.Linq;
 
 namespace charleroi.UI
 {
-	class CPlayerMenu : Panel {
-		public CPlayerMenu() {
-			SetTemplate( "/client/UI/CPlayerMenu.html" );
+	class CPlayerMenu : Panel
+	{
+		public CPlayerMenu()
+		{
+			//SetTemplate( "/client/UI/CPlayerMenu.html" );
+			StyleSheet.Load( "/client/UI/CPlayerMenu.scss" );
+			var top = Add.Panel( "top" );
+			{
+				var tabs = top.AddChild<ButtonGroup>();
+				tabs.AddClass( "navbar" );
+
+				var body = top.Add.Panel( "body" );
+				{
+					tabs.Add.Button( "Buttom 1" );
+					tabs.Add.Button( "Buttom 2" );
+				}
+			}
 		}
 
-		public override void Tick() {
+		public override void Tick()
+		{
 			base.Tick();
 
-			if ( Input.Pressed( InputButton.Menu ) ) {
+			if ( Input.Pressed( InputButton.Menu ) )
+			{
 				var avatar = Parent.ChildrenOfType<CPlayerHUD>().SingleOrDefault();
 				bool status = HasClass( "hidden" );
 
-				if ( status ) {
+				if ( status )
+				{
 					SetClass( "hidden", !status );
 					avatar?.SetClass( "hidden", status );
 				}
-				else {
+				else
+				{
 					SetClass( "hidden", !status );
 					avatar?.SetClass( "hidden", status );
 				}
