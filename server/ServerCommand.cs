c811306ef664b298dc3700f6340f3c80715f28ff
Messorem <jeromegardette@gmail.com>
2021-11-21 07:51:52 +0100

    ajouté cmd pour spawn prop

--- server/ServerCommand.cs
+++ server/ServerCommand.cs
@@ -2,6 +2,7 @@
 
 using charleroi.server.DAL;
 using charleroi.client;
+using charleroi.client.WorldUI;
 
 namespace charleroi.server
 {
@@ -60,6 +61,27 @@ namespace charleroi.server
 				Log.Info( "Player not deleted" );
 			}
 		}
+		
+		[ServerCmd( "rp_spawn" )]
+		public static void Cmd_spawn()
+		{
+			string model = "models/tsx/table_craft.vmdl";
+			string tagname = "Table de craft";
 
+			var client = ConsoleSystem.Caller?.Pawn;
+
+			var tr = Trace.Ray( client.EyePos, client.EyePos + client.EyeRot.Forward * 100 )
+				.UseHitboxes()
+				.Ignore( client )
+				.Run();
+
+			var ent = new Prop();
+			ent.Position = tr.EndPos;
+			ent.Rotation = Rotation.From( new Angles( 0, client.EyeRot.Angles().yaw, 0 ) ) * Rotation.FromAxis( Vector3.Up, 180 );
+			ent.SetModel( model );
+			ent.Position = tr.EndPos - Vector3.Up * ent.CollisionBounds.Mins.z;
+			ent.Name = tagname;
+			ent.Tags.Add( "show" );
+		}
 	}
 }
