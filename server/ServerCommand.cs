78ffe593d2707e4217e484102e5ccf3b04ca81d2
Steve Zaretti <kossolax@gmail.com>
2021-11-21 17:33:06 +0100

    première entité custom
    @DevilJho-io @Hellhium @messoremTsX

--- server/ServerCommand.cs
+++ server/ServerCommand.cs
@@ -3,6 +3,7 @@
 using charleroi.server.DAL;
 using charleroi.client;
 using charleroi.client.WorldUI;
+using System.Linq;
 
 namespace charleroi.server
 {
@@ -61,9 +62,9 @@ namespace charleroi.server
 				Log.Info( "Player not deleted" );
 			}
 		}
-		
-		[ServerCmd( "rp_spawn" )]
-		public static void Cmd_spawn()
+
+		[ServerCmd( "rp_spawn_old" )]
+		public static void Cmd_spawnold()
 		{
 			string model = "models/tsx/table_craft3.vmdl";
 			string tagname = "Table de craft";
@@ -83,15 +84,37 @@ namespace charleroi.server
 			ent.Name = tagname;
 			ent.Tags.Add( "show" );
 
-			var mavariable = ent.GetAttachment( "light" ); // recup de la position de l'attache sur le props
-			var light = new OrthoLightEntity(  ); // création de la lumiere
-			light.Transform = mavariable.Value; // encapsulation de la position
-			light.SetParent( ent ); // attache lumiere avec l'attache du props
-			light.SetLightBrightness(0.008f); // réglage luminosité
-			light.SetLightColor( new Color( 0xF2, 0xBE, 0x5C ) ); // réglage couleur
-			//float OrthoLightHeight { 0.1f };
-			//float OrthoLightWidth {  };
+			var lightPosition = ent.GetAttachment( "light" ); // recup de la position de l'attache sur le props
+			if ( lightPosition.HasValue )
+			{
+				var light = new SpotLightEntity();                      // création de la lumiere
+				light.Transform = lightPosition.Value;                  // encapsulation de la position
+				light.SetParent( ent );                                 // attache lumiere avec l'attache du props
+				light.SetLightBrightness( 0.8f );                       // réglage luminosité
+				light.SetLightColor( new Color( 0xF2, 0xBE, 0x5C ) );   // réglage couleur
+				light.Range = 128.0f;
+			}
+		}
 
+		[ServerCmd( "rp_spawn" )]
+		public static void Cmd_spawn()
+		{
+			Entity.All.Where( i => i.Tags.Has( "show" ) ).ToList().ForEach( i => i.Delete() );
+
+			var client = ConsoleSystem.Caller?.Pawn;
+			var tr = Trace.Ray( client.EyePos, client.EyePos + client.EyeRot.Forward * 100 )
+				.UseHitboxes()
+				.Ignore( client )
+				.Run();
+
+			var ent = new CEntityCrafttable {
+				description = "CECI EST UNE TABLE"
+			};
+
+			ent.Rotation = Rotation.From( new Angles( 0, client.EyeRot.Angles().yaw, 0 ) ) * Rotation.FromAxis( Vector3.Up, 180 );
+			ent.Position = tr.EndPos - Vector3.Up * ent.CollisionBounds.Mins.z;
+
+			ent.TurnOff();
 		}
 	}
 }
---
+++ shared/CEntityCrafttable.cs
@@ -0,0 +1,94 @@
+﻿using charleroi.client;
+using charleroi.client.WorldUI;
+using Sandbox;
+using System;
+using System.Collections.Generic;
+
+namespace charleroi
+{
+	[Library( "tsx_crafttable" )]
+	partial class CEntityCrafttable : Prop
+	{
+		public static class CEntityCrafttableType {
+			public static string none = "models/tsx/table_craft3.vmdl";
+		}
+		private IList<Entity> lightings { get; set; } = new List<Entity>();
+
+		[Net]
+		public string description { get; set; } = "Table de craft V3";
+		private Nametag tag = null;
+
+		public CEntityCrafttable()  {
+			
+		}
+
+		public override void Spawn() {
+			SetModel( CEntityCrafttableType.none );
+			Tags.Add( "show" );
+
+			for(int i=0; i<8; i++) {
+				Transform? pos;
+				pos = GetAttachment( "spotlight" + (i > 0 ? i : "") );
+				if ( pos.HasValue ) {
+					var light = new SpotLightEntity();
+					light.Transform = pos.Value;
+					light.SetParent( this );
+					light.SetLightBrightness( 0.8f );
+					light.SetLightColor( new Color( 255, 255, 64 ) );
+					light.Range = (Math.Abs( CollisionBounds.Mins.z ) + Math.Abs( CollisionBounds.Maxs.z )) * 2;
+					lightings.Add( light );
+				}
+
+				pos = GetAttachment( "light" + (i > 0 ? i : "") );
+				if ( pos.HasValue ) {
+					var light = new PointLightEntity();
+					light.Transform = pos.Value;
+					light.SetParent( this );
+					light.SetLightBrightness( 0.8f );
+					light.SetLightColor( new Color( 255, 255, 64 ) );
+					light.Range = (Math.Abs( CollisionBounds.Mins.z ) + Math.Abs( CollisionBounds.Maxs.z )) * 2;
+					lightings.Add( light );
+				}
+			}
+
+			base.Spawn();
+		}
+
+		public override void ClientSpawn() {
+			base.ClientSpawn();
+		}
+
+		[Input]
+		public void TurnOn() {
+			foreach(var ent in lightings) {
+				if( ent is SpotLightEntity spot )
+					spot.TurnOn();
+				if ( ent is PointLightEntity point )
+					point.TurnOn();
+			}
+		}
+		[Input]
+		public void TurnOff() {
+			foreach ( var ent in lightings ) {
+				if ( ent is SpotLightEntity spot )
+					spot.TurnOff();
+				if ( ent is PointLightEntity point )
+					point.TurnOff();
+			}
+		}
+
+		[Event.Tick.Client]
+		private void ClientTick() {
+			Client cl = Local.Client;
+			float dist = 128.0f;
+
+			if ( tag == null && cl.Pawn.Transform.Position.Distance( Position ) <= dist ) {
+				tag = new Nametag( this, description );
+			}
+			if ( tag != null && cl.Pawn.Transform.Position.Distance( Position ) > dist ) {
+				tag.Delete();
+				tag = null;
+			}
+		}
+	}
+}
