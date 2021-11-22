72177dcd2d3bb5ab45d10be33c85d055db019e17
Steve Zaretti <kossolax@gmail.com>
2021-11-22 09:03:21 +0100

    add custom behavior

--- client/WorldUI/CraftableTAG.cs
+++ client/WorldUI/CraftableTAG.cs
@@ -6,9 +6,20 @@ namespace charleroi.client.WorldUI
 {
 	partial class CraftableTAG : Nametag
 	{
+
 		public CraftableTAG( Entity ent, string text ) : base( ent, text )
 		{
 			SetTemplate( "/client/WorldUI/CraftableTAG.html" );
+		}
+
+		public override void Tick() {
+
+			var table = entity as CEntityCrafttable;
+			if ( table != null ) {
+				// TODO, label, progression...
+			}
+
+			base.Tick();
 		}
 	}
 }
