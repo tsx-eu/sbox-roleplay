cd31959bd9f524ca2e32c7b8e5954ecd050ee39c
kossolax <kossolax@gmail.com>
2021-11-24 13:51:48 +0100

    exemple ClientRPC
    @DevilJho-io @Hellhium

--- client/UI/CPlayerMenuCraft.cs
+++ client/UI/CPlayerMenuCraft.cs
@@ -1,11 +1,37 @@
-﻿using Sandbox;
+﻿using System.Threading.Tasks;
+using Sandbox;
 using Sandbox.UI;
 
 namespace charleroi.client.UI
 {
-	class CPlayerMenuCraft : Panel {
+	public partial class CPlayerMenuCraft : Panel {
+
+		public static CPlayerMenuCraft Instance { get; private set; }
+
 		public CPlayerMenuCraft() {
 			SetTemplate( "/client/UI/CPlayerMenuCraft.html" );
+		}
+
+		[ClientRpc]
+		public static void Show()
+		{
+			if ( Instance == null ) {
+				Instance = Local.Hud.AddChild<CPlayerMenuCraft>();
+				_ = Instance.HideLayer();
+			}
+		}
+
+		public async Task HideLayer() {
+			await Task.DelaySeconds( 10.0f );
+			CPlayerMenuCraft.Hide();
+		}
+
+		public static void Hide()
+		{
+			if ( Instance != null ) {
+				Instance.Delete();
+				Instance = null;
+			}
 		}
 	}
 }
