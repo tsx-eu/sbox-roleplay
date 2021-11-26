c5a38fddb9b4f13f644e092c311e12e33764b806
kossolax <kossolax@gmail.com>
2021-11-26 16:11:48 +0100

    fix pos

--- client/UI/Widget/CDragAndDrop.cs
+++ client/UI/Widget/CDragAndDrop.cs
@@ -8,6 +8,7 @@ namespace charleroi.client.UI
 	{
 		Panel fake;
 		private bool isDragging;
+		private Vector2 pos;
 
 		public CDragAndDrop()
 		{
@@ -26,10 +27,10 @@ namespace charleroi.client.UI
 			base.OnMouseUp( e );
 		}
 
-		public void OnDrag()
-		{
+		public void OnDrag() {
 			isDragging = true;
 			fake = Local.Hud.AddChild<Panel>();
+			CloneTo( this, fake);
 		}
 
 		public void OnDrop() {
@@ -37,26 +38,33 @@ namespace charleroi.client.UI
 			fake.Delete();
 		}
 
+		private void CloneTo(Panel src, Panel dst) {
+			dst.Style.BackgroundColor = Color.Red;
+			dst.Style.BackgroundColor = src.Style.BackgroundColor;
+
+			foreach ( Panel item in src.Children ) {
+				var p = dst.Add.Panel();
+				dst.AddChild( p );
+
+				CloneTo( item, p );
+			}
+		}
 
 
-
-		public override void Tick()
-		{
+		public override void Tick() {
 			base.Tick();
+			 
+			if( isDragging && fake != null ) {
+				pos = Local.Hud.ScreenPositionToPanelPosition( Mouse.Position );
 
-			if( isDragging ) {
-				var pos = Local.Hud.MousePosition;
-				pos = Local.Hud.PanelPositionToScreenPosition( pos );
-				
+				fake.Style.PixelSnap = 1;
 				fake.Style.Position = PositionMode.Absolute;
-				fake.Style.Width = Length.Pixels( Box.ClipRect.width );
-				fake.Style.Height = Length.Pixels( Box.ClipRect.height );
-				fake.Style.Top = Length.Pixels( pos.y );
-				fake.Style.Left = Length.Pixels( pos.x );
-				fake.Style.BackgroundColor = Color.Red;
+				fake.Style.Width = Box.ClipRect.width;
+				fake.Style.Height = Box.ClipRect.height;
+				fake.Style.Top = pos.y * Local.Hud.ScaleFromScreen;
+				fake.Style.Left = pos.x * Local.Hud.ScaleFromScreen;
 				fake.Style.ZIndex = 99999999;
 			}
-
 		}
 	}
 }
