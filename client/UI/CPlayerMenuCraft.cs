5ff6234b03a1eda4d45e507724cc0bb24357ed77
kossolax <kossolax@gmail.com>
2021-12-09 19:36:41 +0100

    fix craft

--- client/UI/CPlayerMenuCraft.cs
+++ client/UI/CPlayerMenuCraft.cs
@@ -13,7 +13,9 @@ namespace charleroi.client.UI
 		private CEntityCrafttable ent;
 		private TimeSince LastRay;
 
-		public CPlayerMenuCraft() : base() {
+		public CPlayerMenuCraft() : base()
+		{
+			AddPage( "craft_change", "Craft", () => PageContainer.AddChild<CPlayerCraft>() );
 			AddPage( "price_change", "Inventaire", () => PageContainer.AddChild<CPlayerInventory>() );
 			AddPage( "card_travel", "Métiers", () => PageContainer.AddChild<CPlayerJob>() );
 
@@ -65,34 +67,4 @@ namespace charleroi.client.UI
 
 	}
 
-	class CPlayerCraftList : Panel
-	{
-		public CPlayerCraftList()								// <CPlayerCraftList>, si das le HTML y'a déjà une classe, il le récup automiquement.
-		{
-			var player = Local.Client.Pawn as CPlayer;
-
-			DeleteChildren( true );
-			foreach ( var craft in Game.Instance.Crafts )
-			{
-				var panel = AddChild<Panel>( "Craft" );             // <div class="Craft">
-				if ( craft.Level > player.CurrentXP )
-					panel.AddClass( "nolvl" );
-
-				var img = panel.AddChild<Panel>( "img" );               // <div class="img"></div>
-				if ( craft.Level > player.CurrentXP )
-					img.AddChild<Label>( "lvl" ).Text = craft.Level.ToString();
-
-				var item = panel.AddChild<Panel>( "item" );				// <div class="item">
-
-				var name = item.AddChild<Panel>( "name" );					// <div class="name">
-				var labelName = name.AddChild<Label>( );					// <label>
-				labelName.Text = craft.Name;								// Acide citrique 2
-
-				var description = item.AddChild<Panel>( "description" );	// <div class="description">
-				var labelDescription = description.AddChild<Label>();		// <label>
-				labelDescription.Text = craft.Description;                  // L'acide citrique est un acide tricarboxylique
-			}
-		}
-
-	}
 }
---
+++ client/UI/MenuNav/CPlayerCraft.cs
@@ -0,0 +1,52 @@
+﻿using Sandbox;
+using Sandbox.UI;
+
+namespace charleroi.client.UI.MenuNav
+{
+	class CPlayerCraft : Panel
+	{
+
+		public CPlayerCraft()
+		{
+			SetTemplate( "/client/UI/MenuNav/CPlayerCraft.html" );
+		}
+
+		public override void Tick()
+		{
+
+		}
+
+	}
+
+
+	class CPlayerCraftList : Panel
+	{
+		public CPlayerCraftList()                               // <CPlayerCraftList>, si das le HTML y'a déjà une classe, il le récup automiquement.
+		{
+			var player = Local.Client.Pawn as CPlayer;
+
+			DeleteChildren( true );
+			foreach ( var craft in Game.Instance.Crafts )
+			{
+				var panel = AddChild<Panel>( "Craft" );             // <div class="Craft">
+				if ( craft.Level > player.CurrentXP )
+					panel.AddClass( "nolvl" );
+
+				var img = panel.AddChild<Panel>( "img" );               // <div class="img"></div>
+				if ( craft.Level > player.CurrentXP )
+					img.AddChild<Label>( "lvl" ).Text = craft.Level.ToString();
+
+				var item = panel.AddChild<Panel>( "item" );             // <div class="item">
+
+				var name = item.AddChild<Panel>( "name" );                  // <div class="name">
+				var labelName = name.AddChild<Label>();                 // <label>
+				labelName.Text = craft.Name;                                // Acide citrique 2
+
+				var description = item.AddChild<Panel>( "description" );    // <div class="description">
+				var labelDescription = description.AddChild<Label>();       // <label>
+				labelDescription.Text = craft.Description;                  // L'acide citrique est un acide tricarboxylique
+			}
+		}
+
+	}
+}
