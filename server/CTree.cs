ad946df35faa71625fcd27376636e3d23ecb3354
kossolax <kossolax@gmail.com>
2021-12-25 20:46:57 +0100

    fix double creation

--- server/CTree.cs
+++ server/CTree.cs
@@ -7,18 +7,15 @@ namespace charleroi.server
 {
 	public partial class CTree : Entity
 	{
-		public List<CTreeLog> logs { get; set; }
+		[Net] public IList<CTreePart> logs { get; set; }
 		[Net] public float Ratio { get; set; } = 1.0f;
 		[Net] public int Slice { get; set; } = 8;
 		[Net] public float Delta { get; set; } = 8.0f;
 		[Net] public Vector3 Size { get; set; }
 		[Net] public int Fork { get; set; } = 1;
 
-
 		public CTree() {
-			logs = new List<CTreeLog>();
 		}
-
 
 		public override void Spawn() {
 			CreateMesh();
@@ -26,13 +23,13 @@ namespace charleroi.server
 
 		private void CreateMesh() {
 			Host.AssertServer();
-
-			logs?.ForEach( i => i.Delete() );
-
+			logs?.ToList().ForEach( i => {
+				i.Delete();
+			});
 
 			var builder = new CTreeBuilder( this );
 			logs = builder.Build();
-			logs.ForEach( i => i.Spawn() );
+			logs?.ToList().ForEach( i => i.Spawn());
 		}
 	}
 
@@ -57,14 +54,11 @@ namespace charleroi.server
 			lastDirection = Vector3.Zero;
 		}
 
-		public List<CTreeLog> Build() {
-			var logs = new List<CTreeLog>();
-
+		public List<CTreePart> Build() {
+			var logs = new List<CTreePart>();
 
 			for ( int i = start; i < stop; i++ ) {
-
-				if ( fork > 0 && i >= config.Slice / 2 )
-				{
+				if ( fork > 0 && i >= config.Slice / 2 ) { // TODO: mid entre start et stop
 					var f = Fork( i );
 					logs.AddRange( f.Build() );
 
@@ -74,13 +68,11 @@ namespace charleroi.server
 					d.z = 0f;
 					d = d / d.Length * config.Delta * 2f;
 					d.z = Rand.Float( -config.Delta, config.Delta );
-					logs.Add( AddLayer( i, d ) );
+					logs.AddRange( AddLayer( i, d ) );
 				}
-				else
-				{
-					logs.Add( AddLayer( i ) );
+				else {
+					logs.AddRange( AddLayer( i ) );
 				}
-
 			}
 
 			return logs;
@@ -96,7 +88,9 @@ namespace charleroi.server
 			return builder;
 		}
 
-		private CTreeLog AddLayer( int i, Vector3? direction = null ) {
+		private List<CTreePart> AddLayer( int i, Vector3? direction = null ) {
+			var ret = new List<CTreePart>();
+
 			float a = MathX.LerpTo( 1, config.Ratio, (float)(i + 0) / config.Slice );
 			float b = MathX.LerpTo( 1, config.Ratio, (float)(i + 1) / config.Slice );
 
@@ -105,6 +99,7 @@ namespace charleroi.server
 				d = d / d.Length * config.Delta;
 				d.x = (lastDirection.x + d.x) / 2.0f;
 				d.y = (lastDirection.y + d.y) / 2.0f;
+				// Vector3.Reflect
 				d.z = Rand.Float( -config.Delta, config.Delta );
 				direction = d;
 			}
@@ -112,23 +107,37 @@ namespace charleroi.server
 			if ( i == start )
 				firstDirection = direction.Value;
 
-			var log = new CTreeLog();
-			log.SrcSize = new Vector2( config.Size.x * a, config.Size.y * a );
-			log.DstSize = new Vector2( config.Size.x * b, config.Size.y * b );
-			log.Height = sliceHeight;
-			log.Position = lastPos + lastDirection + Vector3.Up * sliceHeight;
-			log.Slice = 12;
-			log.Direction = direction.Value;
+			var log = new CTreeLog() {
+				SrcSize = new Vector2( config.Size.x * a, config.Size.y * a ),
+				DstSize = new Vector2( config.Size.x * b, config.Size.y * b ),
+				Height = sliceHeight,
+				Position = lastPos + lastDirection + Vector3.Up * sliceHeight,
+				Slice = 12,
+				Direction = direction.Value,
+				RenderColor = Color.Random,
+				Parent = config
+			};
 
 			if ( i == stop - 1 )
 				log.TopCap = true;
 			if ( i == 0 )
 				log.BotCap = true;
 
+			ret.Add( log );
+
+			if ( i > config.Slice / 2 ) {
+				var stick = new CTreeStick {
+					Position = log.Position,
+					Parent = log
+				};
+				ret.Add( stick );
+			}
+
+
 			lastPos = log.Position;
 			lastDirection = direction.Value;
 
-			return log;
+			return ret;
 		}
 	}
 
@@ -136,7 +145,7 @@ namespace charleroi.server
 	{
 		[Net, Change( nameof( CreateMesh ) )] public bool TopCap { get; set; } = false;
 		[Net, Change( nameof( CreateMesh ) )] public bool BotCap { get; set; } = false;
-		[Net, Change( nameof( CreateMesh ) )] public int Slice { get; set; } = 3;
+		[Net, Change( nameof( CreateMesh ) )] public int Slice { get; set; } = 6;
 		[Net, Change( nameof( CreateMesh ) )] public float Height { get; set; } = 32.0f;
 		[Net, Change( nameof( CreateMesh ) )] public Vector3 Direction { get; set; } = Vector3.Up;
 
@@ -173,14 +182,15 @@ namespace charleroi.server
 		protected virtual Mesh BuildMesh() { return null; }
 		protected virtual Vector3[] BuildCollider() { return null; }
 
-		protected static Vector3 GetCircleVector( int i, int tessellation ) {
+		protected static Vector3 GetCircle( int i, int tessellation )
+		{
 			var angle = i * (float)Math.PI * 2 / tessellation;
 
 			var dx = (float)Math.Cos( angle );
 			var dy = (float)Math.Sin( angle );
 
 			var v = new Vector3( dx, dy, 0 );
-			return v.Normal;
+			return v;
 		}
 		protected static Vector2 Planar( Vector3 pos, Vector3 uAxis, Vector3 vAxis ) {
 			return new Vector2()
@@ -208,13 +218,63 @@ namespace charleroi.server
 	{
 		protected override Mesh BuildMesh() {
 			var mesh = new Mesh( Material.Load( "materials/dev/reflectivity_30.vmat" ) );
+			var Size = new Vector2( 2, 2 );
+
+			var verts = new List<SimpleVertex>();
+			var indices = new List<int>();
+			var a = Vector3.VectorAngle( Direction );
+
+			for ( int i = 0; i <= Slice; i++ )
+			{
+				var normal = GetCircle( i, Slice ).Normal;
+				var texCoord = new Vector2( (float)i / Slice, 0.0f );
+
+				var pos = normal + Vector3.Up * Height;
+				pos.x *= Size.x / 2;
+				pos.y *= Size.y / 2;
+				pos += Direction;
+				GetTangentBinormal( normal, out Vector3 u, out Vector3 v );
+
+				verts.Add( new SimpleVertex()
+				{
+					normal = normal,
+					position = pos,
+					tangent = u,
+					texcoord = texCoord
+				} );
+
+				pos = normal + Vector3.Zero * Height; // Vector3.Down
+				pos.x *= Size.x / 2;
+				pos.y *= Size.y / 2;
+				texCoord.y = 1.0f;
+				verts.Add( new SimpleVertex()
+				{
+					normal = normal,
+					position = pos,
+					tangent = v, // u ?
+					texcoord = texCoord
+				} );
+			}
+
+			for ( int i = 0; i < Slice; i++ )
+			{
+				indices.Add( i * 2 );
+				indices.Add( i * 2 + 1 );
+				indices.Add( i * 2 + 2 );
+
+				indices.Add( i * 2 + 1 );
+				indices.Add( i * 2 + 3 );
+				indices.Add( i * 2 + 2 );
+			}
+
+			mesh.CreateVertexBuffer<SimpleVertex>( verts.Count, SimpleVertex.Layout, verts.ToArray() );
+			mesh.CreateIndexBuffer( indices.Count, indices.ToArray() );
 
 			return mesh;
 		}
 	}
 
-	public partial class CTreeLog : CTreePart
-	{
+	public partial class CTreeLog : CTreePart {
 		[Net, Change( nameof( CreateMesh ) )] public Vector2 SrcSize { get; set; }
 		[Net, Change( nameof( CreateMesh ) )] public Vector2 DstSize { get; set; }
 
@@ -238,7 +298,7 @@ namespace charleroi.server
 			var indices = new List<int>();
 
 			for ( int i = 0; i <= Slice; i++ ) {
-				var normal = GetCircleVector( i, Slice );
+				var normal = GetCircle( i, Slice ).Normal;
 				var texCoord = new Vector2( (float)i / Slice, 0.0f );
 
 				var pos = normal + Vector3.Up * Height;
@@ -318,7 +378,7 @@ namespace charleroi.server
 
 			// Create cap vertices.
 			for ( int i = 0; i < tesselation; i++ ) {
-				var pos = GetCircleVector( i, tesselation ) + normal * height;
+				var pos = GetCircle( i, tesselation ).Normal + normal * height;
 				pos.x *= size.x / 2;
 				pos.y *= size.y / 2;
 				pos += direction;
