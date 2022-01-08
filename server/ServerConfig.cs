5b33eec523952a6832556fd052d85f158867f00d
Steve ZARETTI <kossolax@gmail.com>
2022-01-08 08:16:32 +0100

    updated url

--- server/ServerConfig.cs
+++ server/ServerConfig.cs
@@ -2,7 +2,7 @@
 {
 	class ServerConfig
 	{
-		public static string WSUrl { get; } = "wss://wsstore.stone.leethium.fr/ws";
+		public static string WSUrl { get; } = "wss://api.zaretti.be/ws";
 		public static string WSUser { get; } = "api";
 		public static string WSPass { get; } = "api";
 	}
