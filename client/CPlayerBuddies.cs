d90e86130f42d5616885823328c02b6e307b4e34
Jemy SCHNEPP <dev@leethium.fr>
2022-01-10 10:53:29 +0100

    WIP - Buddies

---
+++ client/CPlayerBuddies.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using charleroi.client.UI;
+using charleroi.client.WorldUI;
+using Sandbox;
+
+namespace charleroi.client
+{
+	public partial class CPlayerBuddies : List<SBuddy>
+	{
+		
+	}
+}
---
+++ shared/POCO/SBuddy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sandbox;
+using charleroi.server.DAL;
+
+
+namespace charleroi
+{
+	[Library]
+	public interface SBuddy : IStorable
+	{
+		public ulong BuddySteamID { get; set; }
+		public SBuddyPerms Flags { get; set; }
+
+	}
+
+	[Flags]
+	public enum SBuddyPerms
+	{
+		None = 0,
+		Root = 1
+	}
+
+
+	[Library]
+	public class ListOfSBuddy : List<SBuddy>
+	{
+	}
+}
